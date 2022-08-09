b6dd0d021a1c89e7758680868b0b12d4bfc40cd8
1222r52egyejt <826030519@qq.com>
2022-08-09 12:40:14 +0800

    完成对约瑟夫环人名信息文件的修改和保存

--- Graphical interface/traverse_by_step_graphical_interface_3.py
+++ Graphical interface/traverse_by_step_graphical_interface_3.py
@@ -1,10 +1,9 @@
+from fileinput import filename
 from PySide2.QtWidgets import QApplication, QMessageBox
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import QFile
 from json import dump
 from PySide2.QtWidgets import QFileDialog
-from json import load
-from json_zip import classB
 import os
 from collections import deque
 path = os.path.realpath(os.curdir)#获取当前目录的绝对路径
@@ -13,51 +12,32 @@ import sys
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
-# self.ui = QUiLoader().load(path+'/ui/main.ui')
-# print(path)
-# print(classB.json_reader())
 class Stats(QWidget):
 
-    def __init__(self,parent=None):
-       
-# 从文件中加载UI定义  
+    def __init__(self):
         qfile_stats = QFile(path+'/Desktop/qiyao/Graphical interface/stats.ui')
         qfile_stats.open(QFile.ReadOnly)
         qfile_stats.close
-        # 从 UI 定义中动态 创建一个相应的窗口对象
-        # 注意：里面的控件对象也成为窗口对象的属性了
-        # 比如 self.ui.button , self.ui.textEdit
+    
         self.ui = QUiLoader().load(qfile_stats)
-
-        # self.ui.button.clicked.connect(self.handleCalc)
         self.ui.Button.clicked.connect(self.traverse)
         self.ui.pushButton.clicked.connect(self.getFiles)
+        self.ui.pushButton_4.clicked.connect(self.saveFiles)
            
     def traverse(self):
     # def traverse(self, stepNum, start_id):
-    
-    # #保存存入文件路径
-    #     filePath, _  = QFileDialog.getSaveFileName(
-    #     self.ui,             # 父窗口对象
-    #     "保存文件", # 标题
-    #     r"d:\\data",        # 起始目录
-    #     "json类型 (*.json)" # 选择类型过滤项，过滤内容在括号中
-    # )
-    # #创建并写入
-    #     with open(filePath, "w") as json_file:
-    #         json_dict = dump(dict, json_file)
 
-        info = self.ui.TextEdit.toPlainText()
-        stepNum = self.ui.spinBox.value()
-        start_id = self.ui.spinBox_2.value()
+        info = self.ui.TextEdit.toPlainText() #获取文本框中的内容
+        stepNum = self.ui.spinBox.value() #获取步长
+        start_id = self.ui.spinBox_2.value() #获取起始位置
         traverse_data = info.splitlines()
         for i in range(len(traverse_data)):
             traverse_data[i] = traverse_data[i].strip()
             
-        # print(traverse_data)
+    
         delData = deque()
         traverse_data = deque(traverse_data)
-    # print(traverse_data)
+
         if 0 <= start_id < len(traverse_data):
             traverse_data.rotate(-(start_id) - 1)
         elif start_id >= len(traverse_data):
@@ -70,18 +50,17 @@ class Stats(QWidget):
         
         while len(traverse_data) > 1:
             traverse_data.rotate(-(stepNum-1))  # 左移stepNum-1次
-            # traverse_data.rotate(-(3-1))  # 左移stepNum-1次
             delData.append(traverse_data[0])
             traverse_data.popleft()  # 删除第stepNum元素
             result = traverse_data[0].split()
-        # return (result)  
+       
 
         QMessageBox.about(self.ui,
                             '统计结果',
                             f'''淘汰顺序为：\n{list(delData)}
                             \n最后剩下的是：\n{result}'''
                             )
-
+        # print(type(info))  
 
     def getFiles(self):
         #实例化QFileDialog
@@ -101,11 +80,27 @@ class Stats(QWidget):
                 #接受读取的内容，并显示到多行文本框中
                 data=f.read()
 
-                self.ui.lineEdit.setText(str(filenames))
+                self.ui.lineEdit.setText(filenames[0])
                 self.ui.TextEdit.setPlainText(data)
+        # print(filenames[0])
+    def saveFiles(self):
+        #实例化QFileDialog
+        dig=QFileDialog()
+        #设置可以打开任何文件
+        dig.setFileMode(QFileDialog.AnyFile)
+        #文件过滤
+        dig.setFilter(QDir.Files)
 
-        # def add_name(self):
-        #     self.ui.TextEdit.appendPlainText('你好，白月黑羽')
+        if dig.exec_():
+            #接受选中文件的路径，默认为列表
+            filenames=dig.selectedFiles()
+        info = self.ui.TextEdit.toPlainText() #获取文本框中的内容
+        dict = info
+        # with open(filenames[0], "w") as json_file:
+        #     json_dict = dump(dict, json_file)
+        with open(filenames[0], "w", encoding='utf-8')as f:
+            f.write(dict)
+        # print(json_dict)
        
 if __name__ == '__main__':
     # app = QApplication([])
